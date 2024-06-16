098099d4820a719869c0bc8bd4e33dfa20f92334
Kyrylo <kirillmedvedev279@gmail.com>
2024-06-16 13:35:25 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobSearchApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
